00684014b709fc4a437e0cf011b62fee5e47a3ba
ashraf-kabir <ashrafkabir95@gmail.com>
2026-05-14 17:49:25 +0600

    Update tsup.config.js

--- tsup.config.js
+++ tsup.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   entry: ['src/index.js'],
   format: ['cjs', 'esm'],
-  external: ['react'],
-  jsx: 'react',
+  external: ['react', 'react/jsx-runtime'],
+  jsx: 'react-jsx',
 });
